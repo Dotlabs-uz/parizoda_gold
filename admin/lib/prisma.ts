448f69f1123ba532beb5b1d22fc545011feecf97
ShukurovOzodbek <shukurov.ozodbek.jaloliddinovich@gmail.com>
2025-08-29 16:02:24 +0500

    fix(prisma): generated prisma client to @prisma/client

--- admin/lib/prisma.ts
+++ admin/lib/prisma.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "../app/generated/prisma";
-import { withAccelerate } from "@prisma/extension-accelerate";
+import {withAccelerate} from "@prisma/extension-accelerate";
+import {PrismaClient} from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
-	prisma: PrismaClient;
+    prisma: PrismaClient;
 };
 
 const prisma =
-	globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+    globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
